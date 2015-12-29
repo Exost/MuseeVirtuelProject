5e489da70d67dbb766cb3fc45eddeeedda0e4348
clementcisterne <lamouche444@hotmail.fr>
2015-12-30 00:11:35 +0100

    Update controllerMessage.php

--- controller/controllerMessage.php
+++ controller/controllerMessage.php
@@ -5,20 +5,36 @@ require ("{$ROOT}{$DS}model{$DS}modelMessage.php");
 switch ($action){
 
 	case 'readAll':
-	
-
+		$layout = "Membre" ;
+		$view   = "All" ;
 	break;
 
 	case 'read':
-
+		$layout = "Membre" ;
+		$view   = "" ;
+		
 	break;
 
 	case 'delete':
 
 	break;
 
-	case 'send':
 
+	case 'send': 
+	
+	if(isset($_POST['texte']) && (isset(_POST['auteur']) && (isset($_POST['destinataire']) )
+		
+		if( exist($_POST['destinataire']) ) // si le destinataire existe
+		{
+			$layout = "Membre" ;
+			$view   = "" ;
+		
+			$texte 		= $_POST['texte'];
+			$auteur		= $_POST['auteur'];
+			$destinataire 	= $_POST['destinataire'];			
+		}
+		
+		
 	break;
 
 }
