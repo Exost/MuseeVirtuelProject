3057e6f79a4aab324d623d26c2083bd9e98e333c
clementcisterne <lamouche444@hotmail.fr>
2015-12-29 23:10:54 +0100

    Create controllerMessage.php

---
+++ controller/controllerMessage.php
@@ -0,0 +1,25 @@
+<?php
+require ("{$ROOT}{$DS}model{$DS}modelMessage.php");
+
+
+switch ($action){
+
+	case 'readAll':
+	
+
+	break;
+
+	case 'read':
+
+	break;
+
+	case 'delete':
+
+	break;
+
+	case 'send':
+
+	break;
+
+}
+?>
