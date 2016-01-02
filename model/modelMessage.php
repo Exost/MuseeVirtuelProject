d3d0a932689d0db228df21a5c9d1aec2dd698001
clementcisterne <lamouche444@hotmail.fr>
2016-01-02 03:33:51 +0100

    Ajouts des fichers correspondant aux fonctionnalités de la messagerie

--- model/modelMessage.php
+++ model/modelMessage.php
@@ -12,22 +12,32 @@ class modelMessage extends Model{
 	private $destinataire;
 	private $texte ;
 	private $etat;
-	private $datemessage;
+	private $dateMessage;
 
 
-	public function __construct($idmessage=NULL, $auteur=NULL, $texte=NULL, $destinataire=NULL, $etat=NULL, $datemessage=NULL)
+	public function __construct($idmessage=NULL, $auteur=NULL, $texte=NULL, $destinataire=NULL, $etat=NULL, $dateMessage=NULL)
 	{
 		if( !is_null($idmessage) && !is_null($auteur) && !is_null($texte)
-			&& !is_null($destinataire) && !is_null($etat) && !is_null($datemessage) )
+			&& !is_null($destinataire) && !is_null($etat) && !is_null($dateMessage) )
 		{
-			$this->idmessage 	= $idmessage ;
-			$this->auteur 	 	= $auteur ;
-			$this->texte 	 	= $texte ;
-			$this->destinataire = $destinataire ;
-			$this->etat 		= $etat ;
-			$this->datemessage 		= $datemessage ;
+			$this->idmessage 		= $idmessage ;
+			$this->auteur 	 		= $auteur ;
+			$this->texte 	 		= $texte ;
+			$this->destinataire	 	= $destinataire ;
+			$this->etat 			= $etat ;
+			$this->dateMessage 		= $dateMessage ;
 		}
 	}
+
+
+	public function getAuteur()			{return $this->auteur ;}
+	public function getTexte()			{return $this->texte ;}
+	public function getIdMessage()	  	{return $this->idmessage ;}
+	public function getDestinataire()	{return $this->destinataire ;}
+	public function getEtat() 			{return $this->etat ;}
+	public function getDateMessage() 	{return $this->dateMessage ;}
+
+
 
 	public static function getMessageRecueByIdMembre($id){
 		$sql='SELECT * FROM '.static::$table.' WHERE destinataire=:id';
@@ -41,16 +51,6 @@ class modelMessage extends Model{
 		}
 		catch(PDOException $e){ if(Conf::getDebug()) { echo $e->getMessage(); } die(); }
 	}
-
-	public function getAuteur()			{return $this->auteur ;}
-	public function getTexte()			{return $this->texte ;}
-	public function getIdMessage()	  	{return $this->idmessage ;}
-	public function getDestinataire()	{return $this->destinataire ;}
-	public function getEtat() 			{return $this->etat ;}
-	public function getDateMessage() 	{return $this->datemessage ;}
-
-
-
 
 
 
