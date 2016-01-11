acfa5e66da5a7800a72b5eb537f5894cb4516eef
Exost <elvy66@live.fr>
2016-01-11 19:20:46 +0100

    CSS membre

--- view/membre/viewProfilMembre.php
+++ view/membre/viewProfilMembre.php
@@ -1,31 +1,32 @@
 <?php if (!isset($_GET['idMembre'])){
 
 
-/**
- * Created by PhpStorm.
- * User: enzo
- * Date: 17/12/15
- * Time: 17:03
- */
 $profilMembre = modelMembre::select($_SESSION['login']);
 
 ?>
+<div id="divMain" style="margin-top: 9em">
+    <div id="imgProfil">
+        <img src='ressources/img/profil.png' />
+    </div>
 
-    <ul>
-        <li><b> <?php echo $profilMembre->getLogin(); ?></b></li>
-        <li><?php echo $profilMembre->getNom(); ?></li>
-        <li> <?php echo $profilMembre->getPrenom(); ?></li>
-        <li><?php echo $profilMembre->getAdresseMail(); ?></li>
-        <li><?php
-            if($profilMembre->getSexe()== 'h'){
-                echo 'homme';
-            }else{
-                echo 'femme';
-            }
-            ?>
-        </li>
-    </ul>
-
+    <div id="infoProfil">
+        <ul>
+            <li class="monProfil"><b> Login </b> :  <?php echo $profilMembre->getLogin(); ?></li>
+            <li class="monProfil"><b> Nom Utilisateur </b>:  <?php echo $profilMembre->getNom(); ?></li>
+            <li class="monProfil"> <b>Prénom </b>:  <?php echo $profilMembre->getPrenom(); ?></li>
+            <li class="monProfil"><b> Adresse Mail </b>:  <?php echo $profilMembre->getAdresseMail(); ?></li>
+            <li class="monProfil"> <b>Sexe :</b> <?php
+                if($profilMembre->getSexe()== 'h'){
+                    echo 'homme';
+                }else{
+                    echo 'femme';
+                }
+                ?>
+            </li>
+        </ul>
+        <a href="index.php?controller=membre&action=modifier" id="modifProfil">Modifier mon Profil ?</a>
+    </div>
+</div>
 
 <?php }else{
 
