71b3287bd57da41d1c9092570e8d543a2e10b3da
enzo <enzo.fabre@orange.fr>
2016-01-12 01:23:11 +0100

    modification rang

---
+++ model/notation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enzo
+ * Date: 11/01/16
+ * Time: 21:06
+ */
+include_once ("model.php");
+class notation {
+    function notation($idDocument, $login){
+
+    }
+}
--- view/document/viewAllDocument.php
+++ view/document/viewAllDocument.php
@@ -1,23 +1,30 @@
-<form method="post" action="index.php?controller=admin&action=suppressionDoc">
-    <table>
-        <?php require_once ("{$ROOT}{$DS}model{$DS}modelDocument.php");
-        /**
-         * Created by PhpStorm.
-         * User: enzo
-         * Date: 29/12/15
-         * Time: 02:49
-         */
-        foreach(modelDocument::getAll() as $document){;
-            echo "<tr>";
-            $titre = $document->getTitre();
-            $id = $document->getIdDocument();
-            echo "<td><a href='index.php?controller=document&action=consulter&idDocument=$id'>$titre</a></td>";
-            if(isset($_SESSION['rang'])&& $_SESSION['rang']=='admin'){
-                echo "<td> <input type='submit' value='supprimer' class='button'/> </td>";
-            }
-            echo "</tr>";
+<div id="allDoc">
+    <form method="post" action="index.php?controller=admin&action=suppressionDoc">
+        <table>
+            <?php require_once ("{$ROOT}{$DS}model{$DS}modelDocument.php");
+            /**
+             * Created by PhpStorm.
+             * User: enzo
+             * Date: 29/12/15
+             * Time: 02:49
+             */
+            foreach(modelDocument::getAll() as $document){;
 
-        } ?>
-    </table>
-</form>
+                echo "<tr>";
+                $titre = $document->getTitre();
+                $id = $document->getIdDocument();
+                $type  = $document->getType();
+                echo "<td></td>";
+                echo "<td><img src='ressources/img/document/$type.png' alt='iconde $type' style='width: 30px'/>
+                <a href='index.php?controller=document&action=consulter&idDocument=$id'>$titre</a></td>";
+                if(isset($_SESSION['rang'])&& $_SESSION['rang']=='admin'){
+                    echo "<td> <input type='submit' value='supprimer' class='button'/> </td>";
+                }
+                echo "</tr>";
+
+            } ?>
+        </table>
+    </form>
+</div>
+
 
